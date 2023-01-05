eaebad93201f508e592c4b2fb2fdf767e2085e4d
francojsantillan <jesusfrancosantillan@gmail.com>
2023-01-05 20:01:31 -0300

    Header and Banner

--- ProyectoFinal_FrontEnd/src/app/app.module.ts
+++ ProyectoFinal_FrontEnd/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgentinaProgramaComponent } from './components/logo-argentina-programa/logo-argentina-programa.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { TitleComponent } from './components/title/title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgentinaProgramaComponent,
+    SocialMediaComponent,
+    BannerComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
